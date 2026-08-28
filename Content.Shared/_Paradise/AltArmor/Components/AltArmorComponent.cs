8ca4901e865f8e01db0226d294d350d6ab5d370c
ANT-14THEFIRST <anewtwinktwink@gmail.com>
2026-08-28 19:06:17 +0300

    Update AltArmorComponent.cs
    Signed-off-by: ANT-14THEFIRST <anewtwinktwink@gmail.com>

--- Content.Shared/_Paradise/AltArmor/Components/AltArmorComponent.cs
+++ Content.Shared/_Paradise/AltArmor/Components/AltArmorComponent.cs
@@ -5,38 +5,36 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Paradise.AltArmor.Components;
 
-[RegisterComponent]
-[NetworkedComponent, AutoGenerateComponentState]
-public partial class AltArmorComponent : Component
+public abstract partial class AltArmorComponent : Component
 {
     /// <summary>
     /// The amount of damage blocked for the user, by damage type.
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public Dictionary<ProtoId<DamageTypePrototype>, FixedPoint2> UserDamageReductionByType = new();
 
     /// <summary>
     /// The amount of damage blocked when the armor itself takes damage, by damage type.
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public Dictionary<ProtoId<DamageTypePrototype>, FixedPoint2> SelfDamageReductionByType = new();
 
     /// <summary>
     /// Specifies what types of damage should be converted to others
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public Dictionary<ProtoId<DamageTypePrototype>, ProtoId<DamageTypePrototype>> TransformSpecifierDict = new Dictionary<ProtoId<DamageTypePrototype>, ProtoId<DamageTypePrototype>>();
 
     /// <summary>
     /// Whether the protection provided by this entity decreases as the entity takes damage.
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public bool DamageAffectsProtection = false;
 
     /// <summary>
     /// The total damage at which the protection provided by this entity is reduced to zero.
     /// Protection decreases linearly as the entity's damage approaches this value.
     /// </summary>
-    [DataField, AutoNetworkedField]
+    [DataField]
     public int ZeroProtectionThreshold = 100;
 }
